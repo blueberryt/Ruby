1a0bca406be7c69f19027fa688435da3a2a99d80
blueberryt <supikateriburu@gmail.com>
2015-07-09 08:55:06 +0900

    Issue#2 simple_match4_answer.rb に対する解答のコミット

---
+++ garan2.rb
@@ -0,0 +1,25 @@
+pattern = Regexp.new(
+  "(.{0,10})(#{ARGV[0]})(.{0,10})"
+)
+
+filename = ARGV[1]
+count = 0
+File.open(filename) do |file|
+  file.each_line do |line|
+    line.scan(pattern) do |matched|
+      count += 1
+      prefix_length = 0
+      matched[0].each_char do |ch|
+        if ch.ord < 128  #=> 半角
+          prefix_length += 1
+        else  #=> 全角
+          prefix_length += 2
+        end
+      end
+      space_length = 20 - prefix_length
+      spaces = " " * space_length
+      puts "#{spaces}#{matched[0]}<<#{matched[1]}>>#{matched[2]}"
+    end
+  end
+end
+puts count
